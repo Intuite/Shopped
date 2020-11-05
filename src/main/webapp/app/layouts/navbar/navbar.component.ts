1a0385b639f901ecca3b1e40ed63e25635deca88
Sebastian Soto Madrigal <ssotom@ucenfotec.ac.cr>
2020-11-05 10:47:44 -0600

    fix(nav): jhilang missing import

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import { AccountService } from 'app/core/auth/account.service';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { LoginService } from 'app/core/login/login.service';
 import { ProfileService } from 'app/layouts/profiles/profile.service';
+import { JhiLanguageService } from 'ng-jhipster';
 
 @Component({
   selector: 'jhi-navbar',
