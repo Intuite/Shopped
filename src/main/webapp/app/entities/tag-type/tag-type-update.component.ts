a5a60c4faa0d01f18891aab6e3e014441cb2a13b
Sebastian Soto Madrigal <ssotom@ucenfotec.ac.cr>
2020-11-06 17:21:09 -0600

    feat(tag-type): update or create form with material angular

--- src/main/webapp/app/entities/tag-type/tag-type-update.component.ts
+++ src/main/webapp/app/entities/tag-type/tag-type-update.component.ts
@@ -14,6 +14,7 @@ import { TagTypeService } from './tag-type.service';
 })
 export class TagTypeUpdateComponent implements OnInit {
   isSaving = false;
+  statusOptions = ['ACTIVE','INACTIVE','BLOCKED','PENDING'];
 
   editForm = this.fb.group({
     id: [],
