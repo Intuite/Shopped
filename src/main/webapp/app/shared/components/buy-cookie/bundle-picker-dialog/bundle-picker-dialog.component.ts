be577e6d29dba2e2e34c6b819764e022e4af5f98
Nicolas <nicolas01001110@gmail.com>
2020-11-11 14:31:43 -0600

    fix(bundle navbar and component): fixed issue regarding navbar and bundle vis

---
+++ src/main/webapp/app/shared/components/buy-cookie/bundle-picker-dialog/bundle-picker-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-bundle-picker-dialog',
+  templateUrl: './bundle-picker-dialog.component.html',
+  styleUrls: ['./bundle-picker-dialog.component.scss'],
+})
+export class BundlePickerDialogComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
