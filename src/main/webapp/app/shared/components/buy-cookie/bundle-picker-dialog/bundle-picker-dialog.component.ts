7d61fb19df48c210c3cc1294869cf6628a4a03b5
Nicolas <nicolas01001110@gmail.com>
2020-11-11 21:40:04 -0600

    fix(dialog component)

--- src/main/webapp/app/shared/components/buy-cookie/bundle-picker-dialog/bundle-picker-dialog.component.ts
+++ src/main/webapp/app/shared/components/buy-cookie/bundle-picker-dialog/bundle-picker-dialog.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Bundle } from 'app/shared/model/bundle.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/user/account.model';
+import { Cookies } from 'app/shared/model/cookies.model';
+import { CookiesService } from 'app/entities/cookies/cookies.service';
 
 @Component({
   selector: 'jhi-bundle-picker-dialog',
@@ -6,7 +14,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bundle-picker-dialog.component.scss'],
 })
 export class BundlePickerDialogComponent implements OnInit {
-  constructor() {}
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  bundleId = 0;
+  bundle = new Bundle();
+  account?: Account;
+  cookie?: Cookies;
 
-  ngOnInit(): void {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Bundle[],
+    private _formBuilder: FormBuilder,
+    private accountService: AccountService,
+    private cookieService: CookiesService
+  ) {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.accountService.identity().subscribe(res => (this.account = res || undefined));
+    this.cookieService.query().subscribe(res => (this.cookie = res.body![0] || undefined));
+    console.warn(this.account?.login);
+    console.warn(this.cookie?.amount);
+  }
+
+  selectBundle(s?: Bundle, stepper?: MatStepper): void {
+    if (s) {
+      this.bundle = s;
+    }
+  }
+
+  /*getBundle(id: number): void {
+    this.service.find(id).subscribe((res: any) => this.bundle = res.body || new Bundle());
+
+  }*/
 }
