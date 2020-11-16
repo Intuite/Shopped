6fafca7a989605344aa9e7b40784e2dc5636c93e
Rogelio <rsanchezc@ucenfotec.ac.cr>
2020-11-16 14:30:19 -0600

    fix: grid layout image size for changes

--- src/main/webapp/app/entities/post/post-detail.component.ts
+++ src/main/webapp/app/entities/post/post-detail.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { Subscription } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { JhiDataUtils } from 'ng-jhipster';
 import { IPost } from 'app/shared/model/post.model';
+import { Recipe, IRecipe } from 'app/shared/model/recipe.model';
+import { RecipeService } from 'app/entities/recipe/recipe.service';
 
 @Component({
   selector: 'jhi-post-detail',
@@ -9,11 +13,32 @@ import { IPost } from 'app/shared/model/post.model';
 })
 export class PostDetailComponent implements OnInit {
   post: IPost | null = null;
+  recipe?: Recipe[];
+  eventSubscriber?: Subscription;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected recipeService: RecipeService, protected dataUtils: JhiDataUtils, protected activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ post }) => (this.post = post));
+    this.recipeService.find(3).subscribe(
+      (res: HttpResponse<IRecipe[]>) => this.onSuccess(res.body),
+      () => this.onError()
+    );
+  }
+
+  protected onSuccess(data: Recipe[] | null): void {
+    this.recipe = data || [];
+  }
+
+  protected onError(): void {
+    console.warn('There was an error');
+  }
+  byteSize(base64String: string): string {
+    return this.dataUtils.byteSize(base64String);
+  }
+
+  openFile(contentType = '', base64String: string): void {
+    this.dataUtils.openFile(contentType, base64String);
   }
 
   previousState(): void {
