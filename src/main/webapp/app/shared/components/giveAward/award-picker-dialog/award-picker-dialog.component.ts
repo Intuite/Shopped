68b764357f6e8aa8eb0709a51d163d2f6bca463a
Nicolas <nicolas01001110@gmail.com>
2020-11-17 22:35:13 -0600

    feat(modal graphical requirments)

--- src/main/webapp/app/shared/components/giveAward/award-picker-dialog/award-picker-dialog.component.ts
+++ src/main/webapp/app/shared/components/giveAward/award-picker-dialog/award-picker-dialog.component.ts
@@ -1,11 +1,13 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { Award, IAward } from 'app/shared/model/award.model';
 import { FormBuilder } from '@angular/forms';
-import { CookieService } from 'ngx-cookie-service';
 import { AccountService } from 'app/core/auth/account.service';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HttpResponse } from '@angular/common/http';
 import { AwardService } from 'app/entities/award/award.service';
+import { Account } from 'app/core/user/account.model';
+import { Cookies, ICookies } from 'app/shared/model/cookies.model';
+import { CookiesService } from 'app/entities/cookies/cookies.service';
 
 @Component({
   selector: 'jhi-award-picker-dialog',
@@ -17,13 +19,14 @@ export class AwardPickerDialogComponent implements OnInit {
   award = new Award();
   account?: Account;
   awards: Award[] = [];
+  cookie = new Cookies();
 
   constructor(
     protected service: AwardService,
     @Inject(MAT_DIALOG_DATA) public data: number,
     private _formBuilder: FormBuilder,
     private accountService: AccountService,
-    private cookieService: CookieService
+    private cookieService: CookiesService
   ) {}
 
   ngOnInit(): void {
@@ -31,6 +34,21 @@ export class AwardPickerDialogComponent implements OnInit {
       (res: HttpResponse<IAward[]>) => this.onSuccess(res.body),
       () => this.onError()
     );
+    this.accountService.identity().subscribe(res => (this.account = res || undefined));
+    this.cookieService
+      .query({
+        ...(this.account?.id && { 'userId.equals': this.account?.id }),
+      })
+      .subscribe(
+        (res: HttpResponse<ICookies[]>) => this.onCookieSuccess(res.body || undefined),
+        () => console.warn('se cayo')
+      );
+  }
+
+  onCookieSuccess(data: any): void {
+    if (data !== null && data !== undefined) {
+      this.cookie = data[0];
+    }
   }
 
   private onError(): void {
@@ -38,6 +56,12 @@ export class AwardPickerDialogComponent implements OnInit {
   }
 
   private onSuccess(body: Award[] | null): void {
+    console.warn(body);
     this.awards = body || [];
+    this.award = (body || [])[0];
+  }
+
+  chargeAward(item: Award): void {
+    this.award = item;
   }
 }
