32bdbd25109efc5eabacc50d1790fa5457789f57
Rogelio <rsanchezc@ucenfotec.ac.cr>
2020-11-22 18:17:49 -0600

    fix: recipe card image size

--- src/main/webapp/app/entities/recipe/recipe-list.component.ts
+++ src/main/webapp/app/entities/recipe/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { JhiDataUtils } from 'ng-jhipster';
 
@@ -7,6 +7,8 @@ import { RecipeService } from 'app/entities/recipe/recipe.service';
 import { HttpResponse } from '@angular/common/http';
 import { Account } from 'app/core/user/account.model';
 import { AccountService } from 'app/core/auth/account.service';
+import { IUser } from 'app/core/user/user.model';
+import { UserService } from 'app/core/user/user.service';
 
 @Component({
   selector: 'jhi-recipe-user-list',
@@ -17,15 +19,23 @@ export class RecipeListComponent implements OnInit {
   recipe: IRecipe | null = null;
   recipes: Recipe[] = [];
   account?: Account;
+  user!: IUser;
 
   constructor(
     protected dataUtils: JhiDataUtils,
     protected activatedRoute: ActivatedRoute,
     protected recipeService: RecipeService,
+    protected userService: UserService,
     private accountService: AccountService
   ) {}
 
   ngOnInit(): void {
+    this.accountService.getAuthenticationState().subscribe(account => {
+      if (account) {
+        this.userService.find(account.login).subscribe(user => (this.user = user));
+      }
+    });
+
     this.recipeService.query().subscribe(
       (res: HttpResponse<IRecipe[]>) => this.onSuccess(res.body),
       () => this.onError()
@@ -58,5 +68,11 @@ export class RecipeListComponent implements OnInit {
     console.warn(body);
     this.recipes = body || [];
     this.recipe = (body || [])[0];
+  }
+
+  onSuccessRecipes(data: any): void {
+    if (data !== null && data !== undefined) {
+      this.recipes = data || [];
+    }
   }
 }
