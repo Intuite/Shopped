92ffd9e29ff0cf64bddfaf210691b79ab2d08cda
Rogelio <rsanchezc@ucenfotec.ac.cr>
2020-12-02 14:08:33 -0600

    feat(post): log at created and view

--- src/main/webapp/app/entities/post/post-detail.component.ts
+++ src/main/webapp/app/entities/post/post-detail.component.ts
@@ -9,6 +9,9 @@ import { IRecipeHasRecipeTag } from 'app/shared/model/recipe-has-recipe-tag.mode
 import { IngredientService } from 'app/entities/ingredient/ingredient.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+import { Log } from 'app/shared/model/log.model';
+import { LogService } from 'app/entities/log/log.service';
+import * as moment from 'moment';
 
 interface FullIngredient {
   id?: number;
@@ -37,7 +40,8 @@ export class PostDetailComponent implements OnInit {
     protected ingredientService: IngredientService,
     protected dataUtils: JhiDataUtils,
     protected activatedRoute: ActivatedRoute,
-    protected accountService: AccountService
+    protected accountService: AccountService,
+    private logService: LogService
   ) {}
 
   ngOnInit(): void {
@@ -47,6 +51,7 @@ export class PostDetailComponent implements OnInit {
       () => this.onSuccess(),
       () => this.onError()
     );
+    this.saveHistory();
   }
 
   byteSize(base64String: string): string {
@@ -63,6 +68,19 @@ export class PostDetailComponent implements OnInit {
 
   onError(): void {
     console.warn('There was an error');
+  }
+
+  saveHistory(): void {
+    const description = JSON.stringify({
+      user: this.account?.login,
+      postId: this.post?.id,
+    });
+    this.logService
+      .create(new Log(undefined, description, moment().startOf('minute'), 'Post view', 3, this.account?.login, this.account?.id))
+      .subscribe(
+        () => console.warn('log succesful'),
+        () => console.warn('log failed')
+      );
   }
 
   protected onSuccess(): void {
